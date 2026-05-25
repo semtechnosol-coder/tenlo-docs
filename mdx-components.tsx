8488607f76b889baaac864b316c5e412713af222
semtechnosol-coder <semtechnosol@gmail.com>
2026-05-25 12:35:31 -0400

    fix(types): use MDXComponents type from nextra/mdx in useMDXComponents

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,10 +1,11 @@
 import { useMDXComponents as getThemeComponents } from 'nextra-theme-docs'
+import type { MDXComponents } from 'nextra/mdx'
 import { Step, Steps } from './components/Step'
 import { Callout } from './components/Callout'
 import { Pill } from './components/Pill'
 import { PrettyTable } from './components/PrettyTable'
 
-export function useMDXComponents(components: Record<string, React.ComponentType> = {}) {
+export function useMDXComponents(components: MDXComponents = {}) {
   return {
     ...getThemeComponents(components),
     Step,
